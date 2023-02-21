1bef50ea072b87748639c70329dc1a7f4885b561
Javier Mellado <sol@javiermellado.com>
2023-02-21 22:34:28 +0000

    add middleware to decorate the req object with user

--- routes/user.ts
+++ routes/user.ts
@@ -1,12 +1,51 @@
-import express, { Request, Response } from 'express';
+import express, { NextFunction, Request, Response } from 'express';
 const router = express.Router();
+
+interface User {
+  name: string;
+}
+
+interface UserRequestParams extends Request {
+  user?: User;
+}
 
 router.get('/', (req: Request, res: Response) => {
   res.send('User List');
 });
 
+router.post('/', (req: Request, res: Response) => {
+  res.send('Create user');
+});
+
 router.get('/new', (req: Request, res: Response) => {
   res.send('User new form');
 });
+
+const users = [{ name: 'Kyle' }, { name: 'Sally' }];
+router.param(
+  'id',
+  (
+    req: UserRequestParams,
+    res: Response,
+    next: NextFunction,
+    id: number
+  ) => {
+    req.user = users[id];
+    next();
+  }
+);
+
+router
+  .route('/:id')
+  .get((req: UserRequestParams, res: Response) => {
+    console.log(req.user);
+    res.send(`Get user with Id ${req.params.id}`);
+  })
+  .put((req: Request, res: Response) => {
+    res.send(`Put user with Id ${req.params.id}`);
+  })
+  .delete((req: Request, res: Response) => {
+    res.send(`Deletek user with Id ${req.params.id}`);
+  });
 
 export default router;
