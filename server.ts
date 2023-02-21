a2f0ed49ffc9fb7901389b31ba908d37d3ff2ff9
Javier Mellado <sol@javiermellado.com>
2023-02-21 21:48:13 +0000

    setup linter and prettier

--- server.ts
+++ server.ts
@@ -3,8 +3,8 @@ import express, { Request, Response } from 'express';
 const app = express();
 
 app.set('view engine', 'ejs');
-app.get('/', (req:Request, res:Response)=>{
-    res.render("index", {text: "world"});
-})
+app.get('/', (req: Request, res: Response) => {
+  res.render('index', { text: 'world' });
+});
 
 app.listen(3000);
